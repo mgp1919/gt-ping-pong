539a167d382dc61e5e446a9686ae4bba6e7774d7
Glitch (gtpingpong) <none>
2019-10-26 23:57:53 +0000

    🍱🌓 Checkpoint ./style.css:8062279/1530 ./script.js:8062279/1080 ./index.html:8062279/168

--- script.js
+++ script.js
@@ -30,7 +30,8 @@ function switchColor(id, bid, id2, id3) {
     if(cont == true) {
       button.innerText = "Reopen";
       elem.style.backgroundColor = "red";
-      document.getElementById(id2).innerHTML = new Date().toLocaleTimeString();
+      // document.getElementById(id2).innerHTML = new Date().toLocaleTimeString();
+      document.getElementById(id2).innerHTML = 0000
     }
 
   }
@@ -51,8 +52,32 @@ function alert(id) {
   return false;
 }
 
+var h2 = document.getElementsByTagName('h2')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
 
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
 
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
 
 
 
